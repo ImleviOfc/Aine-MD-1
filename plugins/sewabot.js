81461b727b936ce0c377376ed81e5c51c5ecd988
levi <111189476+ImleviOfc@users.noreply.github.com>
2022-09-04 10:34:45 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -3,30 +3,44 @@ let handler = async (m, { conn }) => {
   let pepe = 'https://telegra.ph/file/2e2eb7d093fec08fd00d4.jpg'
   let baper = await fetch(pepe).then(a => a.buffer())
 
-  let aine = '6289654360447@s.whatsapp.net'
+  let aine = '6285172446692@s.whatsapp.net'
   let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
   let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
   let c = pickRandom([a, b])
   let d = await fetch(c).then(a => a.buffer())
   let prepare = await require('@adiwajshing/baileys').generateWAMessageFromContent(m.key.remoteJid,{listMessage:{
   title: `${await conn.getName(conn.user.jid)}`,
-  description: ` *• SEWA BOT & UP TO PREMIUM •*
-        
-1. Grup / 30 Hari
-Rp. 15.000 Dana
-Rp. 15.000 Pulsa
+  description: `
 
-2. Premium / 30 Hari
-Rp. 10.000 Dana
-Rp. 10.000 Pulsa
+╭━━━━「 *SEWA* 」
+┊⫹⫺ *Hemat:* _3k/grup (1 minggu)_
+┊⫹⫺ *Normal:* _5k/grup (1 bulan)_
+┊⫹⫺ *Standar:* _10k/grup (2 bulan)_
+┊⫹⫺ *Pro:* _35k/grup (4 bulan)_                                                      
+┊⫹⫺ *Vip:* = _65k/grup (6 bulan & 1 bulan *Premium*)_
+╰═┅═━––––––๑
 
-3. Premium + Grup / 30 Hari
-Rp. 20.000 Dana
-Rp. 20.000 Pulsa
+╭━━━━「 *PREMIUM* 」
+┊⫹⫺ *Hemat:* _3k (1 minggu)_
+┊⫹⫺ *Normal:* _10k (1 bulan)_
+┊⫹⫺ *Pro:* _40k (4 bulan)_
+┊⫹⫺ *Vip:* _50k (8 bulan)_                                               
+┊⫹⫺ *Permanent:* = _999k (Unlimited)_
+╰═┅═━––––––๑
 
-wa.me/${owner[0]}
-*Bukan Bot!!!*
-*Owner ${conn.user.name}*
+*⫹⫺ PAYMENT:*
+• *Pulsa:* [085172446692]
+• *Dana:* [085172446692]
+
+–––––– *🐾 Kebijakan* ––––––
+🗣️: Kak, Kok harganya mahal banget?
+💬: Mau tawar menawar? boleh, silahkan chat owner aja
+
+🗣️: Scam ga nih kak?
+💬: Enggalah
+
+▌│█║▌║▌║║▌║▌║█│▌
+
 `,
   buttonText: 'Harga Sesuai Pasaran',
   listType: 2,
@@ -36,7 +50,7 @@ wa.me/${owner[0]}
   products:[{productId:'7487741964584083'}]}],
   headerImage: { productId: '7487741964584083',
   jpegThumbnail: baper },
-  businessOwnerJid: `6289654360447@s.whatsapp.net`
+  businessOwnerJid: `6285172446692@s.whatsapp.net`
   },
   footerText: 'https://github.com/ImleviOfc',
   }},{})
